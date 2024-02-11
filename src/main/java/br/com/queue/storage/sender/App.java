d926a41615f87147f7b53f2c6da03994b9a34201
[CI&T]André Correia <andreoc@ciandt.com>
2024-02-11 18:29:28 -0300

    Commit inicial do projeto[

---
+++ src/main/java/br/com/queue/storage/sender/App.java
@@ -0,0 +1,11 @@
+package br.com.queue.storage.sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
